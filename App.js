bae8a92797744f678cfa508544dec83c2bbbb10f
FrederickKisife <kilafrederick@gmaul.com>
2023-06-07 05:51:01 -0700

    first commit

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppContainer from './src/navigations/AppNavigation';
 
+/*A container that runs the app */
 export default function App() {
   return (
      <AppContainer />
